e0a97a582418502e3ccf9e68ff8165438eb6adb9
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2023-04-04 21:35:29 -0400

    Redid the welcome message

--- commands/testwelcome.js
+++ commands/testwelcome.js
@@ -9,10 +9,6 @@ module.exports = {
     const target = interaction.user;
 
     if (target.id === process.env.CREATOR_ID) {
-      // Emoji Objects
-      const userEmoji = interaction.guild.emojis.cache.find(
-        (emoji) => emoji.name === process.env.USER_ROLE_EMOJI
-      );
       const tempEmoji = interaction.guild.emojis.cache.find(
         (emoji) => emoji.name === process.env.TEMP_ROLE_EMOJI
       );
@@ -20,14 +16,11 @@ module.exports = {
       // Send a message in the welcome channel
       const message = await interaction.client.channels.cache
         .get(process.env.WELCOME_CHANNEL_ID)
-        .send(
-          `React to this message to get your roles!\nSelect ${userEmoji} to be a user.\nSelect ${tempEmoji} to be a member. (Requires a Password.)`
-        );
+        .send(`Select ${tempEmoji} if you know the member password.`);
 
       // React with the proper role emojis
       message
-        .react(userEmoji)
-        .then(() => message.react(tempEmoji))
+        .react(tempEmoji)
         .catch((error) =>
           console.error("One of the emojis failed to react: ", error)
         );
